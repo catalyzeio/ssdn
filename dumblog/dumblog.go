97058022f81ec32c8b21823266912def4064ce80
Adam Leko <adam@catalyze.io>
2015-03-27 12:48:09 -0500

    Add Error log level and use named constants for level checks

--- dumblog/dumblog.go
+++ dumblog/dumblog.go
@@ -7,12 +7,21 @@ import (
 	"os"
 )
 
+const (
+	LvlPanic int = iota // also fatal
+	LvlError
+	LvlWarn
+	LvlInfo
+	LvlDebug
+	LvlTrace
+)
+
 var (
 	verboseFlag *int
 )
 
 func AddFlags() {
-	verboseFlag = flag.Int("v", 3, "logging verbosity level")
+	verboseFlag = flag.Int("v", LvlInfo, "logging verbosity level")
 }
 
 type DumbLogger struct {
@@ -31,44 +40,48 @@ func (l *DumbLogger) Panic(format string, v ...interface{}) {
 }
 
 func (l *DumbLogger) Fatal(format string, v ...interface{}) {
-	if verboseFlag != nil && *verboseFlag < 1 {
-		return
-	}
 	l.log.Print("FATAL - ", l.prefix, ' ', fmt.Sprintf(format, v...))
 	os.Exit(1)
 }
 
+func (l *DumbLogger) Error(format string, v ...interface{}) {
+	if verboseFlag != nil && *verboseFlag < LvlError {
+		return
+	}
+	l.log.Print("ERROR - ", l.prefix, " - ", fmt.Sprintf(format, v...))
+}
+
 func (l *DumbLogger) Warn(format string, v ...interface{}) {
-	if verboseFlag != nil && *verboseFlag < 2 {
+	if verboseFlag != nil && *verboseFlag < LvlWarn {
 		return
 	}
 	l.log.Print("WARN  - ", l.prefix, " - ", fmt.Sprintf(format, v...))
 }
 
 func (l *DumbLogger) Info(format string, v ...interface{}) {
-	if verboseFlag != nil && *verboseFlag < 3 {
+	if verboseFlag != nil && *verboseFlag < LvlInfo {
 		return
 	}
 	l.log.Print("INFO  - ", l.prefix, " - ", fmt.Sprintf(format, v...))
 }
 
 func (l *DumbLogger) IsDebugEnabled() bool {
-	return verboseFlag != nil && *verboseFlag >= 4
+	return verboseFlag != nil && *verboseFlag >= LvlDebug
 }
 
 func (l *DumbLogger) Debug(format string, v ...interface{}) {
-	if verboseFlag == nil || *verboseFlag < 4 {
+	if verboseFlag == nil || *verboseFlag < LvlDebug {
 		return
 	}
 	l.log.Print("DEBUG - ", l.prefix, " - ", fmt.Sprintf(format, v...))
 }
 
 func (l *DumbLogger) IsTraceEnabled() bool {
-	return verboseFlag != nil && *verboseFlag >= 5
+	return verboseFlag != nil && *verboseFlag >= LvlTrace
 }
 
 func (l *DumbLogger) Trace(format string, v ...interface{}) {
-	if verboseFlag == nil || *verboseFlag < 5 {
+	if verboseFlag == nil || *verboseFlag < LvlTrace {
 		return
 	}
 	l.log.Print("TRACE - ", l.prefix, " - ", fmt.Sprintf(format, v...))
