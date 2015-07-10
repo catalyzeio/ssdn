500461f4b3878d6930ea3c85f63cca5671e1f4fa
Adam Leko <adam@catalyze.io>
2015-07-10 08:05:16 -0500

    Use watch support from µDocker client

--- watch/dns.go
+++ watch/dns.go
@@ -17,6 +17,7 @@ import (
 )
 
 const (
+	dockerRetryInterval   = 15 * time.Second
 	registryRetryInterval = 5 * time.Second
 )
 
@@ -41,8 +42,103 @@ func NewContainerDNS(dc *udocker.Client, rc *registry.Client, tenant, runDir, co
 }
 
 func (c *ContainerDNS) Watch() {
-	go c.query()
 	go c.advertise()
+	go c.query()
+}
+
+type serviceSet map[string]locationSet
+type locationSet map[string]struct{}
+
+func (s serviceSet) add(name, location string) {
+	locs, present := s[name]
+	if !present {
+		locs = make(locationSet)
+		s[name] = locs
+	}
+	locs[location] = struct{}{}
+}
+
+func (s serviceSet) toAds() []registry.Advertisement {
+	var ads []registry.Advertisement
+	for name, locs := range s {
+		for loc, _ := range locs {
+			ads = append(ads, registry.Advertisement{
+				Name:     name,
+				Location: loc,
+			})
+		}
+	}
+	return ads
+}
+
+func (c *ContainerDNS) advertise() {
+	var set serviceSet
+
+	dc := c.dc
+	rc := c.rc
+
+	changes := dc.Watch()
+	for {
+		// grab list of containers
+		if log.IsDebugEnabled() {
+			log.Debug("Updating list of containers")
+		}
+		containers, err := dc.ListContainers(false)
+		if err != nil {
+			log.Warn("Error querying list of Docker containers: %s", err)
+			time.Sleep(dockerRetryInterval)
+			continue
+		}
+
+		// refresh advertisements if the current set has changed
+		newSet := c.extractSet(containers)
+		if !reflect.DeepEqual(set, newSet) {
+			ads := newSet.toAds()
+			log.Info("Updating registry advertisements: %s", ads)
+			if err := rc.Advertise(ads); err != nil {
+				log.Warn("Error updating registry: %s", err)
+				time.Sleep(registryRetryInterval)
+				continue
+			}
+			set = newSet
+		}
+
+		// wait for container state changes
+		<-changes
+	}
+}
+
+func (c *ContainerDNS) extractSet(containers []udocker.ContainerSummary) serviceSet {
+	set := make(serviceSet)
+	for _, container := range containers {
+		tenant, present := container.Labels[TenantLabel]
+		// only examine containers belonging to this tenant
+		if !present || tenant != c.tenant {
+			continue
+		}
+		if log.IsTraceEnabled() {
+			log.Trace("Container %s belongs to tenant %s", container.Id, c.tenant)
+		}
+		// check if the container has any service data
+		data, present := container.Labels[ServicesLabel]
+		if !present {
+			continue
+		}
+		// extract service data
+		var services []Service
+		if err := json.Unmarshal([]byte(data), &services); err != nil {
+			log.Warn("Container %s has invalid service definition: %s", container.Id, err)
+			continue
+		}
+		if log.IsTraceEnabled() {
+			log.Trace("Container %s services: %v", container.Id, services)
+		}
+		// add service data to results
+		for _, v := range services {
+			set.add(v.Name, v.Location)
+		}
+	}
+	return set
 }
 
 func (c *ContainerDNS) query() {
@@ -117,99 +213,4 @@ func (c *ContainerDNS) render(enum *registry.Enumeration) error {
 	}
 	log.Info("Updated %s", dataFile)
 	return nil
-}
-
-type serviceSet map[string]locationSet
-type locationSet map[string]struct{}
-
-func (s serviceSet) add(name, location string) {
-	locs, present := s[name]
-	if !present {
-		locs = make(locationSet)
-		s[name] = locs
-	}
-	locs[location] = struct{}{}
-}
-
-func (s serviceSet) toAds() []registry.Advertisement {
-	var ads []registry.Advertisement
-	for name, locs := range s {
-		for loc, _ := range locs {
-			ads = append(ads, registry.Advertisement{
-				Name:     name,
-				Location: loc,
-			})
-		}
-	}
-	return ads
-}
-
-func (c *ContainerDNS) advertise() {
-	var set serviceSet
-
-	dc := c.dc
-	rc := c.rc
-
-	dockerChanges := RateLimitWatch(dc)
-	for {
-		// grab list of containers
-		if log.IsDebugEnabled() {
-			log.Debug("Updating list of containers")
-		}
-		containers, err := dc.ListContainers(false)
-		if err != nil {
-			log.Warn("Error querying list of Docker containers: %s", err)
-			time.Sleep(dockerRetryInterval)
-			continue
-		}
-
-		// refresh advertisements if the current set has changed
-		newSet := c.extractSet(containers)
-		if !reflect.DeepEqual(set, newSet) {
-			ads := newSet.toAds()
-			log.Info("Updating registry advertisements: %s", ads)
-			if err := rc.Advertise(ads); err != nil {
-				log.Warn("Error updating registry: %s", err)
-				time.Sleep(registryRetryInterval)
-				continue
-			}
-			set = newSet
-		}
-
-		// wait for changes to docker
-		<-dockerChanges
-	}
-}
-
-func (c *ContainerDNS) extractSet(containers []udocker.ContainerSummary) serviceSet {
-	set := make(serviceSet)
-	for _, container := range containers {
-		tenant, present := container.Labels[TenantLabel]
-		// only examine containers belonging to this tenant
-		if !present || tenant != c.tenant {
-			continue
-		}
-		if log.IsTraceEnabled() {
-			log.Trace("Container %s belongs to tenant %s", container.Id, c.tenant)
-		}
-		// check if the container has any service data
-		data, present := container.Labels[ServicesLabel]
-		if !present {
-			continue
-		}
-		// extract service data
-		var services []Service
-		if err := json.Unmarshal([]byte(data), &services); err != nil {
-			log.Warn("Container %s has invalid service definition: %s", container.Id, err)
-			continue
-		}
-		if log.IsTraceEnabled() {
-			log.Trace("Container %s services: %v", container.Id, services)
-		}
-		// add service data to results
-		for _, v := range services {
-			set.add(v.Name, v.Location)
-		}
-	}
-	return set
 }
